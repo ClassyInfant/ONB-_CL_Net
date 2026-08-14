1742b4f3bdfa1d50a622dae75a194f3fe34a03db
ClassyInfant <130485147+ClassyInfant@users.noreply.github.com>
2026-08-14 11:06:04 -0400

    Delete scripts/ezlibs-scripts directory
    Testing linking to git

--- scripts/ezlibs-scripts/ezconfig.lua
+++
@@ -1,7 +0,0 @@
-local ezconfig = nil
-print('[ezconfig] loading config...')
-ezconfig = require('ezlibs-config')
-if ezconfig == nil then
-    error('[ezconfig] ezlibs requires a ezlibs-config.lua in the root of your server!')
-end
-return ezconfig
